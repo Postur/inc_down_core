b7e79db71d0a7aeac7a365fcd24169d059ec0376
Postur <Christian@steingrund.com>
2022-05-16 19:08:39 +0100

    ehe

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,78 @@
 use clap::Parser;
 use pulldown_cmark::{html, Options};
-use std::{env, ffi::OsStr, fs, path::Path};
+use std::{env, fs, path::PathBuf};
 
-// #[derive(Parser)]
-// struct Cli {
-//   /// The pattern to look for
-//   #[clap(parse(from_os_str))]
-//   input_file: std::path::PathBuf,
-//   /// The path to the file to read
-//   #[clap(parse(from_os_str))]
-//   output_file: Option<std::path::PathBuf>,
-// }
+#[derive(Parser, Debug)]
+struct Args {
+  /// The pattern to look for
+  #[clap(
+    parse(from_os_str),
+    long,
+    short = 'i',
+    help = "The Markdown file to parse"
+  )]
+  input: std::path::PathBuf,
+  /// The path to the file to read
+  #[clap(
+    parse(from_os_str),
+    long,
+    short = 'o',
+    help = "The output file, defaults to input file .html"
+  )]
+  output: Option<std::path::PathBuf>,
+}
+fn parse(input: PathBuf) -> String {
+  let file_contents = fs::read_to_string(input).expect("Could not read file");
+  // parse file_contents info front_matter and content
 
+  // register plugins from markdown front_matter
+
+  // run content through plugins
+  let contents = file_contents;
+  // return contents
+  contents
+}
 fn main() {
-  for argument in env::args() {
-    println!("{:?}", argument);
-  }
+  let args = Args::parse();
+  let input_file = args.input;
+  println!("Input file: {}", input_file.display());
+  let mut output_file = args.output.unwrap_or(input_file.clone());
+  output_file.set_extension("html");
+  println!("output file: {}", output_file.display());
+  println!(
+    "Relative path from: {}",
+    env::current_dir().unwrap().display()
+  );
 
-  // let args = std::env::split_paths(unparsed: &T)
-  // let args: Vec<String> = env::args().collect();
-  // println!("{:?}", args);
+  println!(
+    "The generated html can be found here: {}",
+    output_file.display()
+  );
+  let content = parse(input_file);
+  let mut parser_options = Options::empty();
+  parser_options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+  let parser = pulldown_cmark::Parser::new_ext(&content, parser_options);
 
-  // let in_file = std::env::args().nth(1).expect("no path given");
+  println!("{:#?}", parser);
 
-  // let in_file = Path::new(&args[1]);
-  // println!("Input file path is : {:?}", in_file.display());
-  // assert_eq!(Some(OsStr::new("md")), in_file.extension());
-
-  let contents =
-    fs::read_to_string(env::args().nth(1).expect("no path given")).expect("Could not read file");
-
-  println!("{}", contents);
-
-  let markdown_input = "# Hello world\nThis is a ~~complicated~~ *very simple* example.";
-
-  // Set up options and parser. Strikethroughs are not part of the CommonMark standard
-  // and we therefore must enable it explicitly.
-  let mut options = Options::empty();
-  options.insert(Options::ENABLE_STRIKETHROUGH);
-  let parser = pulldown_cmark::Parser::new_ext(markdown_input, options);
-
-  // Write to String buffer.
   let mut html_output = String::new();
   html::push_html(&mut html_output, parser);
+  fs::write(output_file, html_output).expect("Failed to write file");
+  return;
+  // let markdown_input = "# Hello world\nThis is a ~~complicated~~ *very simple* example.";
 
-  // Check that the output is what we expected.
-  let expected_html =
-    "<h1>Hello world</h1>\n<p>This is a <del>complicated</del> <em>very simple</em> example.</p>\n";
-  assert_eq!(expected_html, &html_output);
-  println!("{}", html_output);
+  // // Set up options and parser. Strikethroughs are not part of the CommonMark standard
+  // // and we therefore must enable it explicitly.
+  // let mut options = Options::empty();
+  // options.insert(Options::ENABLE_STRIKETHROUGH);
+
+  // // Write to String buffer.
+  // let mut html_output = String::new();
+  // html::push_html(&mut html_output, parser);
+
+  // // Check that the output is what we expected.
+  // let expected_html =
+  //   "<h1>Hello world</h1>\n<p>This is a <del>complicated</del> <em>very simple</em> example.</p>\n";
+  // assert_eq!(expected_html, &html_output);
+  // println!("{}", html_output);
 }
