4b77264fb29fd3be8ed09da48209f25f6b708f39
Postur <Christian@steingrund.com>
2022-06-14 16:40:02 +0100

    eheh

--- markdown/test.md
+++ markdown/test.md
@@ -1,3 +1,15 @@
+---
+plugins:
+  odeon:
+  toc:
+  date:
+  variables:
+    Author: "Chr"
+settings:
+  allow_functions: true
+  allow_raw_html: true
+---
+
 # heading 1.1 {#html_id_attr .class_for_heading}
 
 i am a paragraph in heading 1
@@ -8,8 +20,13 @@ i am a paragraph in heading 1
 ## heading 1.2
 
 paragraph in h2
+gregre
+
+tg4ert
 
 # head 1.1
+{Variables}(Author)
+{odeon_graph}(./somefolder/somepar.par, "agrument2")
 
 ## head 1.2
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use clap::Parser;
-use pulldown_cmark::{html, Options};
-use std::io::Write as _;
+use clap::Parser as clapParser;
+
 use std::{env, fs, path::PathBuf};
 
-#[derive(Parser, Debug)]
+#[derive(clapParser, Debug)]
 struct Args {
   /// The pattern to look for
   #[clap(
@@ -22,26 +21,51 @@ struct Args {
   )]
   output: Option<std::path::PathBuf>,
 }
-fn parse(input: PathBuf) -> String {
-  let file_contents = fs::read_to_string(input).expect("Could not read file");
-  // parse file_contents info front_matter and content
-  // register plugins from markdown front_matter
 
-  // run content through plugins
-  let contents = file_contents;
-  // return contents
-  contents
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::error::Error;
+use pest::Parser;
+#[derive(Parser)]
+#[grammar = "inctex.pest"]
+struct IncDParser;
+
+enum IncNode<'a> {
+  // Node(Vec<(&'a str, IncNode<'a>)>),
+  Text(&'a str),
+  Command(&'a str),
 }
 
-pub enum EventExt<'a> {
-  Event(pulldown_cmark::Event<'a>),
-  FunctionCall,
+struct Command<'a> {
+  name: &'a String,
+  args: [&'a String],
 }
+fn serialize_jsonvalue(val: &IncNode) -> String {
+  use IncNode::*;
 
+  match val {
+    // Node(o) => {
+    //   let contents: Vec<_> = o
+    //     .iter()
+    //     .map(|(name, value)| format!("\"{}\":{}", name, serialize_jsonvalue(value)))
+    //     .collect();
+    //   format!("{{{}}}", contents.join(","))
+    // }
+    Text(s) => format!("\"{}\"", s),
+    Command(s) => format!("\"{}\"", s),
+    // Number(n) => format!("{}", n),
+    // Boolean(b) => format!("{}", b),
+    // Null => format!("null"),
+  }
+}
 fn main() {
+  // Parse arguments
   let args = Args::parse();
   let input_file = args.input;
   println!("Input file: {}", input_file.display());
+  // if no output path is given use default
   let mut output_file = args.output.unwrap_or(input_file.clone());
   output_file.set_extension("html");
   println!("output file: {}", output_file.display());
@@ -49,28 +73,50 @@ fn main() {
     "Relative path from: {}",
     env::current_dir().unwrap().display()
   );
-
   println!(
     "The generated html can be found here: {}",
     output_file.display()
   );
-  let content = parse(input_file);
-  let mut parser_options = Options::empty();
-  parser_options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+  let file_contents = fs::read_to_string(input_file).expect("Could not read file");
+  fn parse_json_file(file: &str) -> Result<IncDParser, Error<Rule>> {
+    let json = IncDParser::parse(Rule::file, file)?.next().unwrap();
+    use pest::iterators::Pair;
 
-  let parser = pulldown_cmark::Parser::new_ext(&content, parser_options).map(|event| match event {
-    pulldown_cmark::Event::Rule => event,
-    _ => {
-      println!("{:#?}", event);
-      event
+    fn parse_value(pair: Pair<Rule>) -> IncDParser {
+      match pair.as_rule() {
+        // Rule::object => IncDParser::Object(
+        //     pair.into_inner()
+        //         .map(|pair| {
+        //             let mut inner_rules = pair.into_inner();
+        //             let name = inner_rules
+        //                 .next()
+        //                 .unwrap()
+        //                 .into_inner()
+        //                 .next()
+        //                 .unwrap()
+        //                 .as_str();
+        //             let value = parse_value(inner_rules.next().unwrap());
+        //             (name, value)
+        //         })
+        //         .collect(),
+        // ),
+        // Rule::array => IncDParser::Array(pair.into_inner().map(parse_value).collect()),
+        Rule::command => IncDParser::Command(pair.into_inner().next().unwrap().as_str()),
+        Rule::text => IncDParser::Text(pair.into_inner().next().unwrap().as_str()),
+        // Rule::number => IncDParser::Number(pair.as_str().parse().unwrap()),
+        // Rule::boolean => IncDParser::Boolean(pair.as_str().parse().unwrap()),
+        // Rule::null => IncDParser::Null,
+        // Rule::json
+        // | Rule::EOI
+        // | Rule::pair
+        // | Rule::value
+        // | Rule::inner
+        // | Rule::char
+        // | Rule::WHITESPACE => unreachable!(),
+      }
     }
-  });
-  // for idk in parser.into_offset_iter().collect::<Vec<_>>() {
-  //   println!("{:#?}", &idk);
-  // }
-
-  let mut html_output = String::new();
-  html::push_html(&mut html_output, parser);
-  fs::write(output_file, html_output).expect("Failed to write file");
+    Ok(parse_value(json))
+    // ...
+  }
   return;
 }
